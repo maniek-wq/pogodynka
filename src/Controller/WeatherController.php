3ad5973c55e1d6a73f4225e7955c84c50cd63552
Jakub Wasilewski <jakwasi01@gmail.com>
2023-12-12 22:33:24 +0100

    lab-f

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -4,14 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Location;
 use App\Repository\MeasurementRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/{id}', name: 'app_weather', requirements: ['id' => '\d+'])]
-    public function city(Location $location, MeasurementRepository $repository): Response
+    #[Route('/weather/{country}/{city}', name: 'app_weather', requirements: ['id' => '\d+'])]
+    public function city(
+        #[MapEntity(mapping: ['country' => 'country', 'city' => 'city'])]
+        Location $location,
+        MeasurementRepository $repository,
+    ): Response
     {
         $measurements = $repository->findByLocation($location);
 
@@ -20,5 +25,4 @@ class WeatherController extends AbstractController
             'measurements' => $measurements,
         ]);
     }
-
 }
